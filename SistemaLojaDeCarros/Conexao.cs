214fb4dccd1838b10e067eaa946c968803f6a0fe
3DUTEX <t3xdev@gmail.com>
2023-11-05 18:00:36 -0300

    Tela inicial e classe Conexao feitas

---
+++ SistemaLojaDeCarros/Conexao.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SistemaLojaDeCarros
+{
+    internal class Conexao
+    {
+        MySqlConnection con = new MySqlConnection(@"Data Source=localhost;Initial Catalog=bdpizzaria; User ID=root; Password=12345678");
+        public static string msg;
+        public MySqlConnection MyConnetarBD()
+        {
+
+            try
+            {
+                con.Open();
+
+            }
+            catch (Exception erro)
+            {
+
+                msg = "Ocorreu um erro ao se conectar: " + erro.Message;
+            }
+            return con;
+        }
+
+        public MySqlConnection MyDeConnectionBD()
+        {
+            try
+            {
+                con.Close();
+
+            }
+            catch (Exception erro)
+            {
+
+                msg = "Ocorreu um erro ao se desconectar: " + erro.Message;
+            }
+            return con;
+        }
+    }
+}
---
+++ SistemaLojaDeCarros/loginTela.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SistemaLojaDeCarros
+{
+    partial class loginTela
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.senhaLabel = new System.Windows.Forms.Label();
+            this.txtBoxLogin = new System.Windows.Forms.TextBox();
+            this.txtBoxSenha = new System.Windows.Forms.TextBox();
+            this.btnLogar = new System.Windows.Forms.Button();
+            this.tituloLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginLabel.Location = new System.Drawing.Point(211, 133);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(62, 24);
+            this.loginLabel.TabIndex = 0;
+            this.loginLabel.Text = "Login:";
+            // 
+            // senhaLabel
+            // 
+            this.senhaLabel.AutoSize = true;
+            this.senhaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.senhaLabel.Location = new System.Drawing.Point(211, 195);
+            this.senhaLabel.Name = "senhaLabel";
+            this.senhaLabel.Size = new System.Drawing.Size(70, 24);
+            this.senhaLabel.TabIndex = 1;
+            this.senhaLabel.Text = "Senha:";
+            // 
+            // txtBoxLogin
+            // 
+            this.txtBoxLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxLogin.Location = new System.Drawing.Point(290, 133);
+            this.txtBoxLogin.Name = "txtBoxLogin";
+            this.txtBoxLogin.Size = new System.Drawing.Size(204, 29);
+            this.txtBoxLogin.TabIndex = 2;
+            // 
+            // txtBoxSenha
+            // 
+            this.txtBoxSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxSenha.Location = new System.Drawing.Point(290, 192);
+            this.txtBoxSenha.Name = "txtBoxSenha";
+            this.txtBoxSenha.Size = new System.Drawing.Size(204, 29);
+            this.txtBoxSenha.TabIndex = 3;
+            // 
+            // btnLogar
+            // 
+            this.btnLogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogar.Location = new System.Drawing.Point(262, 276);
+            this.btnLogar.Name = "btnLogar";
+            this.btnLogar.Size = new System.Drawing.Size(178, 45);
+            this.btnLogar.TabIndex = 4;
+            this.btnLogar.Text = "Logar";
+            this.btnLogar.UseVisualStyleBackColor = true;
+            // 
+            // tituloLabel
+            // 
+            this.tituloLabel.AutoSize = true;
+            this.tituloLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tituloLabel.Location = new System.Drawing.Point(211, 53);
+            this.tituloLabel.Name = "tituloLabel";
+            this.tituloLabel.Size = new System.Drawing.Size(308, 24);
+            this.tituloLabel.TabIndex = 5;
+            this.tituloLabel.Text = "Por favor, entre com o seu usuário..";
+            // 
+            // loginTela
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 413);
+            this.Controls.Add(this.tituloLabel);
+            this.Controls.Add(this.btnLogar);
+            this.Controls.Add(this.txtBoxSenha);
+            this.Controls.Add(this.txtBoxLogin);
+            this.Controls.Add(this.senhaLabel);
+            this.Controls.Add(this.loginLabel);
+            this.Name = "loginTela";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label senhaLabel;
+        private System.Windows.Forms.TextBox txtBoxLogin;
+        private System.Windows.Forms.TextBox txtBoxSenha;
+        private System.Windows.Forms.Button btnLogar;
+        private System.Windows.Forms.Label tituloLabel;
+    }
+}
+
