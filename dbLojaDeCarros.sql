ce2f48d1144c73fa2556bb0339ebf95f9883c85d
3DUTEX <t3xdev@gmail.com>
2023-11-13 10:13:31 -0300

    script banco alterado

--- dbLojaDeCarros.sql
+++ dbLojaDeCarros.sql
@@ -9,18 +9,6 @@ nm_usuario varchar(40) not null,
 no_senha varchar(40) not null
 );
 
--- EXEC 3
-create table Cliente(
-cdg_cliente int primary key auto_increment,
-dt_nasc date not null,
-nm_nome varchar(40) not null,
-nm_sobrenome varchar(40) not null,
-cdg_loginFunc int,
-CONSTRAINT FOREIGN KEY (cdg_loginFunc) REFERENCES loginFunc(cdg_loginFunc),
-cdg_endereco int,
-CONSTRAINT FOREIGN KEY (cdg_endereco) REFERENCES Endereco(cdg_endereco)
-);
-
 -- EXEC 2
 create table Endereco(
 cdg_endereco int primary key auto_increment,
@@ -29,6 +17,18 @@ no_casa varchar(40) not null,
 ds_complemento varchar(40),
 nm_bairro varchar(40) not null,
 nm_cidade varchar(40) not null
+);
+
+-- EXEC 3
+create table Cliente(
+cdg_cliente int primary key auto_increment,
+dt_nasc date not null,
+nm_cliente varchar(40) not null,
+nm_sobrenome varchar(40) not null,
+cdg_loginFunc int,
+CONSTRAINT FOREIGN KEY (cdg_loginFunc) REFERENCES loginFunc(cdg_loginFunc),
+cdg_endereco int,
+CONSTRAINT FOREIGN KEY (cdg_endereco) REFERENCES Endereco(cdg_endereco)
 );
 
 -- EXEC 4
@@ -41,7 +41,7 @@ nm_placa varchar(20) not null,
 imagem varchar(100) not null,
 ds_veiculo varchar(100) not null,
 cdg_loginFunc int,
-FOREIGN KEY (cdg_loginFunc) REFERENCES loginFunc(cdg_loginFunc)
+CONSTRAINT FOREIGN KEY (cdg_loginFunc) REFERENCES loginFunc(cdg_loginFunc)
 );
 
 INSERT INTO loginfunc (nm_usuario, no_senha)
